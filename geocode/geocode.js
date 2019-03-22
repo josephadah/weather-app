3958d18c8d97899880f111c671a560f4ec95eb47
Joseph Adah <jonathanadahjoe@gmail.com>
2019-03-22 16:04:30 +0100

    add api key

--- geocode/geocode.js
+++ geocode/geocode.js
@@ -3,10 +3,13 @@ const request = require('request');
 const geocodeAddress = (address, callback) => {
     const encodedAddress = encodeURI(address);
 
+    console.log(encodedAddress);
     request({
-        url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
+        url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyCXQAgIiy1m3MFeq7ZYn-k6EovbU_oGcUw`,
         json: true
     }, (error, response, body) => {
+        console.log(error);
+        console.log(JSON.stringify());
         if (error) {
             callback('Unable to connect to google server.');
         } else if (body.status === 'EMPTY_RESULTS') {
