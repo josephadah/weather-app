3934ed0ddcbb57c0f33a18a9aee7ad35a9356b6b
Joseph Adah <jonathanadahjoe@gmail.com>
2019-03-22 18:28:15 +0100

    remove api key

--- geocode/geocode.js
+++ geocode/geocode.js
@@ -1,27 +1,30 @@
-const request = require('request');
+const request = require("request");
 
 const geocodeAddress = (address, callback) => {
-    const encodedAddress = encodeURI(address);
+  const encodedAddress = encodeURI(address);
 
-    console.log(encodedAddress);
-    request({
-        url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyCXQAgIiy1m3MFeq7ZYn-k6EovbU_oGcUw`,
-        json: true
-    }, (error, response, body) => {
-        console.log(error);
-        console.log(JSON.stringify());
-        if (error) {
-            callback('Unable to connect to google server.');
-        } else if (body.status === 'EMPTY_RESULTS') {
-            callback('Address not found.');
-        } else if (body.status === 'OK') {
-            callback(undefined, {
-                address: body.results[0].formatted_address,
-                latitude: body.results[0].geometry.location.lat,
-                longitude: body.results[0].geometry.location.lng
-            });
-        }
-    });
-}
+  console.log(encodedAddress);
+  request(
+    {
+      url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=`,
+      json: true
+    },
+    (error, response, body) => {
+      console.log(error);
+      console.log(JSON.stringify());
+      if (error) {
+        callback("Unable to connect to google server.");
+      } else if (body.status === "EMPTY_RESULTS") {
+        callback("Address not found.");
+      } else if (body.status === "OK") {
+        callback(undefined, {
+          address: body.results[0].formatted_address,
+          latitude: body.results[0].geometry.location.lat,
+          longitude: body.results[0].geometry.location.lng
+        });
+      }
+    }
+  );
+};
 
 module.exports.geocodeAddress = geocodeAddress;
